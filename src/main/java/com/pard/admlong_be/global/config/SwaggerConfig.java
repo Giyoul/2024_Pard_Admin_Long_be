e488917cf5c5b9d22f41d6bdcc23e70cfd7583e9
Giyoul <cla12093@gmail.com>
2024-07-25 15:52:52 +0900

    Update Swagger Server

--- src/main/java/com/pard/admlong_be/global/config/SwaggerConfig.java
+++ src/main/java/com/pard/admlong_be/global/config/SwaggerConfig.java
@@ -2,19 +2,22 @@ package com.pard.admlong_be.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-//    @Value("${recordon.server.domain}")
-//    private String domain;
+    @Value("${manageLongkerthon.server.domain}")
+    private String domain;
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-//                .addServersItem(new Server().url(domain))
+                .addServersItem(new Server().url("http://localhost:8080"))
+                .addServersItem(new Server().url(domain))
                 .info(new Info()
                         .title("프로젝트 이름")
                         .description("설명")
